4ea652dfb9a9527b0b3cdb6efd43c4303aa10ffa
Clarence Bakirtzidis <clarenceb@gmail.com>
2014-09-27 23:06:15 +1000

    Added n (num jokes) flag to client

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
 )
@@ -9,11 +10,27 @@ import (
 func main() {
 	fmt.Println("Welcome to the Jokes API client!")
 
+	var n = flag.Int("n", 1, "Number of jokes to get")
+	flag.Parse()
+
+	for i := 0; i < *n; i++ {
+		joke, err := joke()
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("%d: %s\n", i+1, joke)
+	}
+}
+
+func joke() (string, error) {
 	resp, err := http.Get("http://localhost:8080/joke")
 
 	if err != nil {
 		fmt.Println(err)
-		return
+		return "", err
 	}
 
 	var body struct {
@@ -22,8 +39,8 @@ func main() {
 
 	if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
 		fmt.Println(err)
-		return
+		return "", err
 	}
 
-	fmt.Println(body.Joke)
+	return body.Joke, nil
 }
